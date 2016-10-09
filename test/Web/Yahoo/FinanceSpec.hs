674f44a501fee60327d81834ed4fa6a8aecceb92
James Haver <mchaver@gmail.com>
2016-10-09 11:19:07 +0800

    API test passing for all versions of servant

--- test/Web/Yahoo/FinanceSpec.hs
+++ test/Web/Yahoo/FinanceSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Web.Yahoo.FinanceSpec (
@@ -6,26 +7,73 @@ module Web.Yahoo.FinanceSpec (
  ) where
 
 import Network.HTTP.Client.TLS (getGlobalManager)
-import Web.Yahoo.Finance.YQL.Types
 import Test.Hspec  
 import Servant.Client
+import Web.Yahoo.Finance.YQL.Types
+
+#if MIN_VERSION_servant(0,9,0)
+#elif MIN_VERSION_servant(0,5,0)
+import Control.Monad.Except
+#else
+import Control.Monad.Trans.Either
+#endif
 
 main :: IO ()
 main = hspec spec
+
+
+
+{-
+    manager <- reader getHttpManager
+    eitherRes <- liftIO . runExceptT $
+        getQuoteLowLevel
+            yahooFinanceJsonBaseUrl
+            manager
+            stockSymbols
+            (Just "json")
+            (Just "detail")
+
+    eitherRes <- liftIO . runEitherT $
+        getQuoteLowLevel
+            yahooFinanceJsonBaseUrl
+            stockSymbols
+            (Just "json")
+            (Just "detail")
+-}
 
 spec :: Spec 
 spec = do 
   describe "getQuotes" $ do 
     it "should retrieve 'GOOG' stock information (assuming the stock with symbol 'GOOG' exists)" $ do
-      manager <- getGlobalManager
+#if MIN_VERSION_servant(0,9,0)
+      manager <- getGlobalManager  
       res <- runClientM (getQuotes (YQLQuery [StockSymbol "GOOG"]) ) (ClientEnv manager yahooFinanceJsonBaseUrl)
+#elif MIN_VERSION_servant(0,6,0)
+      manager <- getGlobalManager      
+      res <- runExceptT (getQuotes (YQLQuery [StockSymbol "GOOG"]) manager yahooFinanceJsonBaseUrl)
+#elif MIN_VERSION_servant(0,5,0)
+      manager <- getGlobalManager      
+      res <- runExceptT (getQuotes yahooFinanceJsonBaseUrl manager (YQLQuery [StockSymbol "GOOG"]))
+#else
+      res <- runEitherT (getQuotes yahooFinanceJsonBaseUrl (YQLQuery [StockSymbol "GOOG"]))
+#endif
       case res of
         Left  err -> fail $ "Query failed: " ++ show err
         Right qs -> (quoteSymbol . head . responseQuotes $ qs) `shouldBe` "GOOG"
 
     it "should retrieve multiples quotes" $ do
+#if MIN_VERSION_servant(0,9,0)
       manager <- getGlobalManager      
       res <- runClientM (getQuotes (YQLQuery [StockSymbol "GOOG", StockSymbol "AA"]) ) (ClientEnv manager yahooFinanceJsonBaseUrl)
+#elif MIN_VERSION_servant(0,6,0)
+      manager <- getGlobalManager      
+      res <- runExceptT (getQuotes (YQLQuery [StockSymbol "GOOG", StockSymbol "AA"]) manager yahooFinanceJsonBaseUrl)
+#elif MIN_VERSION_servant(0,5,0)
+      manager <- getGlobalManager      
+      res <- runExceptT (getQuotes yahooFinanceJsonBaseUrl manager (YQLQuery [StockSymbol "GOOG", StockSymbol "AA"]))
+#else
+      res <- runEitherT (getQuotes yahooFinanceJsonBaseUrl (YQLQuery [StockSymbol "GOOG", StockSymbol "AA"]))
+#endif
       case res of
         Left  err -> fail $ "Query failed: " ++ show err
         Right qs -> (length . responseQuotes $ qs) `shouldBe` 2
